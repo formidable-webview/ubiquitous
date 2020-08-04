7485f113512d0f28f00cbaf8fd4ac8dba8bfbb39
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-08-04 13:45:30 -0300

    chore: ignore events.ts in coverage
    This file is here to produce synthetic events. Typescript conformance is
    sufficient for a 100% practical coverage.

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
   testRegex: 'src/.*\\.test\\.tsx?$',
-  coveragePathIgnorePatterns: ['/node_modules/', '__tests__']
+  coveragePathIgnorePatterns: ['/node_modules/', '__tests__', 'src/events.ts']
 };
