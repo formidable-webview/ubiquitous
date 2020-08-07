7e080aee098d270d9f9e76091cc8e0b92714f943
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-08-07 09:07:40 -0300

    chore(eslint): remove uncessary webjs rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,6 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   ignorePatterns: ['lib/', 'types/'],
-  overrides: [
-    {
-      files: ['src/**/*.webjs'],
-      extends: ['eslint:recommended', 'plugin:compat/recommended'],
-      parser: 'espree',
-      parserOptions: { ecmaVersion: 5, sourceType: 'script' },
-      env: {
-        browser: true
-      },
-      rules: {
-        'no-unused-vars': 0,
-        strict: ['error', 'never'],
-        '@typescript-eslint/no-unused-vars': 'off',
-        'dot-notation': 'off'
-      }
-    }
-  ],
   rules: {
     'comma-dangle': ['error', 'never'],
     '@typescript-eslint/no-unused-vars': [
